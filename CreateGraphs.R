2b10e4ca0a92e9124b96be4fd3f6517211cbd937
Matt Warren <matt.warren@live.co.uk>
2017-04-13 15:06:31 +0100

    Tidy-up R Script

--- CreateGraphs.R
+++ CreateGraphs.R
@@ -1,18 +1,6 @@
-args<-commandArgs(TRUE)
+args <- commandArgs(TRUE)
 
 data <- read.csv(args[1])
-#print(data)
-
-#png(file = "line_chart.png", width=1200, height=700)
-
-# Plot the bar chart. 
-#plot(data[, 1], type = "p", col = "blue", ylab = 'Throughput/#Threads', xlab = 'Time (sec)', main = 'The Main Title') # throughput
-#lines(data[, 2], type = "l", col = "red")
-
-#legend("topright", legend=c("Throughput", "# Threads"), col=c("blue", "red"), lty=1:2, cex=0.8)
-
-# Save the file.
-#dev.off()
 
 library(ggplot2)
 
